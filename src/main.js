24b7f57eb2f9fcacab86d9e4204c5d92d2947d23
PUBLICSTATICS <wwh1906161174@qq.com>
2021-02-03 11:45:19 +0800

    初始化调整项目解构

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
+/**
+ * 项目入口
+ */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
 
+/**
+ * 创建一个vue实例，通过render方法把App根组件渲染到#app入口节点
+ */
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+// el: '#app' 等价于$mount('#app')
